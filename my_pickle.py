b6394285be222a13d63367efad491e5e06ae284d
sje216 <75685520+sje216@users.noreply.github.com>
2021-01-14 22:56:24 +0900

    update my_pickle.py & profile.pickle
    my_pickle.py 속에 profile.pickle 제목수정

--- my_pickle.py
+++ my_pickle.py
@@ -1,11 +1,11 @@
 import pickle
-# profile_file = open("frofile.pickle", "wb")
-# profile = {"이름":"박명수", "나이":24, "취미":["골프", "스키", "화내기"]}
-# print(profile)
-# pickle.dump(profile, profile_file) #profile 에 있는 정보 profile_file에 저장
-# profile_file.close()
+profile_file = open("profile.pickle", "wb")
+profile = {"이름":"박명수", "나이":24, "취미":["골프", "스키", "화내기"]}
+print(profile)
+pickle.dump(profile, profile_file) #profile 에 있는 정보 profile_file에 저장
+profile_file.close()
 
-profile_file = open("frofile.pickle", "rb")
+profile_file = open("profile.pickle", "rb")
 profile = pickle.load(profile_file)
 print(profile)
 profile_file.close()
